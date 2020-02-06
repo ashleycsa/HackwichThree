d4289d32e2bce1bbb4c7ec9c8c30a9dad20ef804
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-02-06 11:06:26 -1000

    Completed Part 4

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -14,9 +14,13 @@ class ViewController: UIViewController
     var firstString = "The background will turn to blue"
     var secondString = "The background will turn to green"
     
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
-    
-    
+    //problem set 2
+    var Three = 123
+    var Four = 456
+    @IBOutlet weak var thirdLabel: UILabel!
     
     override func viewDidLoad()
     {
@@ -30,12 +34,27 @@ class ViewController: UIViewController
         if firstString == "The background will turn to red"
         {
             self.view.backgroundColor = UIColor.red
+            self.redLabel.text="Red"
         }
+            
         else
         {
             self.view.backgroundColor = UIColor.blue
+            self.blueLabel.text="Blue"
+        }
+        
+    }
+    
+    @IBAction func magicButtonPressed(_ sender: Any)
+    {
+        if Three < Four
+        {
+            self.view.backgroundColor = UIColor.green
+            self.thirdLabel.text="Green"
+            print("I completed both problem sets")
         }
     }
     
+    
+    
 }
-
